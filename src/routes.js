8b30a31a3b23d0004eb61e4a7dfefb975732ab20
Salmo Mascarenhas <salmo.cruz@gmail.com>
2020-10-30 08:42:17 -0300

    resolved cadastro

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
-// import Cadastro from './pages/Cadastro';
 import Login from './pages/Login';
 import Principal from './pages/Main';
 import Atividades from './pages/Atividades';
-
+import Cadastro from './pages/Cadastro';
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route component={Principal} path='/' exact />
                 <Route component={Login} path='/login' />
-                {/* <Route component={Cadastro} path='/cadastro' /> */}
+                <Route component={Cadastro} path='/cadastro' />
                 <Route component={Atividades} path='/atividades' />
             </Switch>
         </BrowserRouter>
